b4e34589243369b67c50813ca2f70052f652d281
Abdu-Lateef <tomiwafadeyi@gmail.com>
2024-06-21 14:55:56 -0500

    Attempt 7 to try and fix deployment issues

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(flash());
